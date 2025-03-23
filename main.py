ebc6235e5c6464e1e8d6aef81b592ea8a865f86d
Iván Recasens <irecasensl@gmail.com>
2025-03-23 17:57:52 +0100

    Player movement, asteroids and collision detection

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
+
 import pygame
 
 import constants
-from constants import *
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 
 def main():
@@ -13,7 +18,18 @@ def main():
     print('Screen height:', constants.SCREEN_HEIGHT)
 
     pygame.init()
+    game_clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatables, drawables)
+    Asteroid.containers = (updatables, drawables, asteroids)
+    AsteroidField.containers = updatables
+
+    player_ship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     # Game loop
     while True:
@@ -23,6 +39,23 @@ def main():
                 return
 
         screen.fill('black')
+        delta_t = game_clock.tick(60) / 1000
+
+        for u in updatables:
+            u.update(delta_t)
+
+        for d in drawables:
+            d.draw(screen)
+
+        for a in asteroids:
+            if a.is_colliding(player_ship):
+                print('Game over!')
+                sys.exit(0)
+
+        # This should work according to the project instructions, why doesn't it?
+        # updatables.update(delta_t)
+        # drawables.draw(screen)
+
         pygame.display.flip()
 
 
